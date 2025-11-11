400a17e988bbf1dd0e1fcb2f4618224b8ee9062d
CcphoK <flavoirkeswa@gmail.com>
2025-11-11 15:26:28 +0200

    exercise 3 week 3

---
+++ ex_3_arrays.js
@@ -0,0 +1,20 @@
+// TODO: Create an empty array called colors
+// TODO: Add three colors to the array using push()
+// TODO: Create another array called numbers with five numbers
+// TODO: Remove the last color from the colors array
+// TODO: Add a new color to the beginning of the colors array
+// TODO: Print the length of both arrays
+let colors = [];
+
+colors.push("pink");
+colors.push("blue");
+colors.push("purple");
+
+let numbers = [5, 10, 15, 20, 25];
+
+colors.pop();
+
+colors.unshift("green")
+
+console.log("Colors array length:", colors.length);
+console.log("Numbers array:", numbers.length);
